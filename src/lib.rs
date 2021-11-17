5056fc2c2ef10a8736d22e1b9b94d3026757d276
Federico Guerinoni <guerinoni.federico@gmail.com>
2021-11-17 18:56:09 +0100

    Add struct of GreenPass
    For now there is only version, and test with a valid json schema.
    
    Signed-off-by: Federico Guerinoni <guerinoni.federico@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,44 @@
+use serde::{Deserialize, Serialize};
+// use serde_json::Result;
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct GreenPass {
+    ver: String,
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::GreenPass;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn validate_json_version() {
+        let data = r#"
+        {
+            "ver": "1.3.0",
+            "nam": {
+              "fn": "Smith-Jones",
+              "fnt": "SMITH<JONES",
+              "gn": "Charles Edward",
+              "gnt": "CHARLES<EDWARD"
+            },
+            "dob": "1964-01-01",
+            "v": [
+              {
+                "tg": "840539006",
+                "vp": "1119349007",
+                "mp": "EU/1/20/1507",
+                "ma": "ORG-100031184",
+                "dn": 1,
+                "sd": 2,
+                "dt": "2021-06-11",
+                "co": "NL",
+                "is": "Ministry of Health Welfare and Sport",
+                "ci": "URN:UVCI:01:NL:DADFCC47C7334E45A906DB12FD859FB7#1"
+              }
+            ]
+          }"#;
+
+        let gp: GreenPass = serde_json::from_str(data).unwrap();
+        assert_eq!(gp.ver, "1.3.0");
     }
 }
